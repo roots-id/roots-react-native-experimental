6f8c2e723c9bd64f59a1a7c18f7085c547b315cb
2byrds <2byrds@gmail.com>
2022-12-05 11:52:28 -0500

    initial commit after getting prism-swift-sdk call to createDID to work from RN app
    Signed-off-by: 2byrds <2byrds@gmail.com>

---
+++ ios/DIDFuncionalities.swift
@@ -0,0 +1,66 @@
+// DIDFuncionalities.swift
+import Builders
+import Domain
+import Foundation
+import PrismAgent
+
+@objc(DIDFuncionalities)
+class DIDFuncionalities: NSObject {
+//  private let castor: Castor
+  private let castor: Castor
+  private let agent: PrismAgent
+  @Published var createdDID: DID?
+  @Published var resolvedDID: DIDDocument?
+  
+  override
+  init()
+  {
+    self.castor = CastorBuilder(
+        apollo: ApolloBuilder().build()
+    ).build()
+    self.agent = PrismAgent()
+  }
+
+ @objc(addEvent:location:date:)
+ func addEvent(_ name: String, location: String, date: NSNumber) -> Void {
+   print("Hello World!",location,date)
+   Task {
+         await createPrismDID()
+     print("DID is",createdDID ?? "DID unset")
+     
+       }
+
+ }
+ 
+  func createPrismDID() async {
+      // Creates new PRISM DID
+      let did = try? await agent.createNewPrismDID(
+          // Add this if you want to provide a IndexPath
+          // keyPathIndex: <#T##Int?#>
+          // Add this if you want to provide an alias for this DID
+          // alias: <#T##String?#>
+          // Add any services available in the DID
+          services: [ .init(
+              id: "DemoID",
+              type: ["DemoType"],
+              serviceEndpoint: .init(uri: "DemoServiceEndpoint")
+          )
+     ])
+      
+      await MainActor.run {
+          self.createdDID = did
+        print("DID is",createdDID ?? "DID unset")
+      }
+  }
+  
+  func resolveDID() async {
+          guard let did = createdDID else { return }
+
+          // Resolves a DID and returns a DIDDocument
+          let document = try? await castor.resolveDID(did: did)
+
+          await MainActor.run {
+              self.resolvedDID = document
+          }
+      }
+  }
